03d75259f872da235298f4513fe3155fcddda7f2
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-17 16:32:38 +0200

    Create sezioni a pagine

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ var typed = new Typed(".typing", {
 // Aside
 const nav = document.querySelector(".nav"),
 navList = nav.querySelectorAll("li"),
-totalNavList = navList.length;
+totalNavList = navList.length,
+allSection = document.querySelectorAll(".section"),
+totalSection = allSection.length;
 for(let i=0; i < totalNavList; i++){
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function(){
@@ -17,5 +19,14 @@ for(let i=0; i < totalNavList; i++){
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add("active");
+        showSection(this);
     })
+}
+
+function showSection(element) {
+    for(let i=0; i < totalSection; i++){
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
 }
