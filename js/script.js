cf84e498c2448090fc90fbf884160546f0f436f3
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-17 14:17:13 +0200

    Implementata cdn per auto typing

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+var typed = new Typed(".typing", {
+    strings:["","Backend Developer","Frontend Developer","Fullstack Web Developer!"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
